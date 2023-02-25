e9dfc418b56aee63f2318c30333c5c453f8ebb2c
Namita N V <105543145+NamitaNV@users.noreply.github.com>
2023-02-25 11:48:18 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector(".navbar");
+ 
+menubar.onclick =() =>{
+    navbar.classList.toggle('open');
+}
+let submenu= document.querySelector(".sub-menu");
+menubar.onclick =() =>{
+   navbar.classList.toggle('open');
+}
